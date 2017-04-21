e170f02f4718000d904da7158b0b9806a97a7abd
vamsikrishna-kolli <vamsi461krishna@gmail.com>
2017-04-21 16:52:06 +0530

    Modified models, forms and views in blog app.

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils import timezone
+
 
 class Reminder(models.Model):
     title = models.CharField(max_length=50)
     message = models.CharField(max_length=50)
-    reminderTime = models.DateTimeField(default=timezone.now())
-    createdTime = models.DateTimeField(auto_now_add=True)
-    modifiedTime = models.DateTimeField(auto_now=True)
+    reminder_time = models.DateTimeField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "Reminder"
