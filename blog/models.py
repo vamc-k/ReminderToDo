3527817498ef843bad092e1d59cf7423c1394922
vamsikrishna-kolli <vamsi461krishna@gmail.com>
2017-04-25 18:02:49 +0530

    Modified models.py with token generation and api's to access token.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.db import models
-
-
-# Create your models here.
-from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Reminder(models.Model):
@@ -11,6 +11,14 @@ class Reminder(models.Model):
     reminder_time = models.DateTimeField()
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey('auth.User', related_name='reminders')
+    highlighted = models.TextField()
 
     class Meta:
         db_table = "Reminder"
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
